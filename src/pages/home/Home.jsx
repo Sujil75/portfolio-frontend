f2f4d618e10c60fdeff462f06572c1c9b8fc05c6
Sujil <sujilsmathew54@gmail.com>
2026-05-21 21:11:54 +0530

    structured home page, details are not filled will be after completing the designing part

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,7 +5,27 @@ import './Style.css'
 
 function Home() {
   return (
-    <div>Home</div>
+    <section className='home-container'>
+        <div className='available-container'>
+            <div className='available-color-container'></div>
+            
+            <strong>AVAILABLE TO WORK</strong>
+        </div>
+
+        <section className='home-profile-section'>
+            <div className='profile-details-container'>
+                <h1 className='profile-name'>Hello, I'm ___</h1>
+
+                <h3 className='profile-roles'>I'm a ___</h3>
+
+                <p className='profile-para'>Crafting premium digital experiences with code, design, and obsessive attention to detail</p>
+                
+                <button type='button' className='resume-btn'>Get Resume</button>
+            </div>
+
+            <img src="" className='profile-img' />
+        </section>
+    </section>
   )
 }
 
