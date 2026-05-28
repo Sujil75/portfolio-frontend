b218aea2007a9a22785493bb5f887dc5ff734575
Sujil <sujilsmathew54@gmail.com>
2026-05-28 21:25:06 +0530

    designed hero/home section, now to add more content and api integration

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -9,21 +9,28 @@ function Home() {
         <div className='glass'></div>
 
         <div className='home-content'>
-            <div className='topic-container'>
-                <div className='available-color-container'></div>
-                
-                <strong>AVAILABLE TO WORK</strong>
+            <div className='available-container'>
+                <div className='available-container-glass'></div>
+
+                <div className='topic-container available-topic'>
+                    <div className='available-notify-container'></div>
+                    
+                    <p>AVAILABLE TO WORK</p>
+                </div>
             </div>
 
             <section className='home-profile-section'>
                 <div className='profile-details-container'>
-                    <h1>Hello, I'm ___</h1>
+                    <h1>Hello, I'm <span>Name</span></h1>
 
-                    <h3>I'm a ___</h3>
+                    <h3>I'm a <span>FullStack Developer</span></h3>
 
                     <p>Crafting premium digital experiences with code, design, and obsessive attention to detail</p>
                     
-                    <button type='button' className='resume-btn'>Get Resume</button>
+                    <button
+                        type='button'
+                        className='resume-btn'
+                    >Get Resume</button>
                 </div>
 
                 <img src="" className='profile-img' alt="profile img" />
