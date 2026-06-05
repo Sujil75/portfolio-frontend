a87930f5e224f0e3b9f02af4e2230bedae59705b
Sujil <sujilsmathew54@gmail.com>
2026-06-05 16:02:01 +0530

    completed navbar mobile approach

--- src/components/cards/MenuCard.jsx
+++ src/components/cards/MenuCard.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import { icons } from '../../assets/icons'
+import { NavLinks } from '../../data/NavLink'
 
 function MenuCard({setMenuBtn}) {
     const CloseBtnIcon = icons.closeBtn
@@ -10,6 +11,18 @@ function MenuCard({setMenuBtn}) {
         <button type='button' onClick={() => setMenuBtn(false)}>
             <CloseBtnIcon size={24} className='mobile-icon' />
         </button>
+
+        <hr />
+
+        <ul className='mobile-nav-content-container'>
+            {
+                NavLinks.map(each => (
+                <li key={each.id} onClick={() => setMenuBtn(false)}>
+                    <a href={each.href} className='nav-content'>{each.content}</a>
+                </li>
+                ))
+            }
+        </ul>
     </section>
     )
 }
